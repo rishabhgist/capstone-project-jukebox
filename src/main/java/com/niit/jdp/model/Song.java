ffdcff5ef6fcb94de08688e45c583690813c21d3
Rishabh <rishabhgupta7199@gmail.com>
2022-09-20 14:35:37 +0530

    added parameterized and no arguments constructor

--- src/main/java/com/niit/jdp/model/Song.java
+++ src/main/java/com/niit/jdp/model/Song.java
@@ -8,4 +8,16 @@ public class Song {
     private String songGenre;
     private double songLength;
     private String songArtist;
+
+    public Song() {
+    }
+
+    public Song(String songName, String songGenre, double songLength, String songArtist) {
+        this.songName = songName;
+        this.songGenre = songGenre;
+        this.songLength = songLength;
+        this.songArtist = songArtist;
+    }
+
+
 }

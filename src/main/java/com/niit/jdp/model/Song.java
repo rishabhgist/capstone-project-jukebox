e03771b208489a1efdc9a7e44cb0e9bdd0a210f7
Rishabh <rishabhgupta7199@gmail.com>
2022-09-20 14:34:41 +0530

    added fields to Song class, added lombok external library

--- src/main/java/com/niit/jdp/model/Song.java
+++ src/main/java/com/niit/jdp/model/Song.java
@@ -1,4 +1,11 @@
 package com.niit.jdp.model;
 
+import lombok.Data;
+
+@Data
 public class Song {
+    private String songName;
+    private String songGenre;
+    private double songLength;
+    private String songArtist;
 }

db55f5763b6194b5e8594070362a9a472392cb91
Rishabh <rishabhgupta7199@gmail.com>
2022-09-20 15:06:31 +0530

    added displayAll and displayById method in PlaylistRepository class

--- src/main/java/com/niit/jdp/repository/PlaylistRepository.java
+++ src/main/java/com/niit/jdp/repository/PlaylistRepository.java
@@ -1,5 +1,19 @@
 package com.niit.jdp.repository;
 
+import java.sql.Connection;
+
 public class PlaylistRepository {
-    
+    // display all playlist
+    public void displayAllPlaylist(Connection connection) {
+    }
+
+    ;
+
+    //display playlist by Id
+    public void displayPlaylistById(Connection connection, int Id) {
+    }
+
+    ;
+
+
 }

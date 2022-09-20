87fed144185887c9594a44e121df81494de70579
Rishabh <rishabhgupta7199@gmail.com>
2022-09-20 14:41:10 +0530

    added fields, constructor, toString, equals and hashcode methods in Playlist

--- src/main/java/com/niit/jdp/model/Playlist.java
+++ src/main/java/com/niit/jdp/model/Playlist.java
@@ -1,4 +1,18 @@
 package com.niit.jdp.model;
 
+import lombok.Data;
+
+@Data
 public class Playlist {
+    private String playlistName;
+    private String songId;
+
+    public Playlist() {
+    }
+
+    public Playlist(String playlistName, String songId) {
+        this.playlistName = playlistName;
+        this.songId = songId;
+    }
+
 }

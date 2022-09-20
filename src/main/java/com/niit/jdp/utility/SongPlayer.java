cb26e5fdeb3a6f27ca397d6e471426aad4712853
Rishabh <rishabhgupta7199@gmail.com>
2022-09-20 14:51:31 +0530

    added File class object and AudioInputStream object

--- src/main/java/com/niit/jdp/utility/SongPlayer.java
+++ src/main/java/com/niit/jdp/utility/SongPlayer.java
@@ -1,4 +1,11 @@
 package com.niit.jdp.utility;
 
+import javax.sound.sampled.AudioInputStream;
+import java.io.File;
+
 public class SongPlayer {
+    File songFile;
+    AudioInputStream audioInputStream;
+
+
 }

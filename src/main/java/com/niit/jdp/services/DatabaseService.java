cf04ae87ca018901b30b4c9ab06567ec2fe9d671
Rishabh <rishabhgupta7199@gmail.com>
2022-09-20 14:45:55 +0530

    added fields to DatabaseService, added connect method

--- src/main/java/com/niit/jdp/services/DatabaseService.java
+++ src/main/java/com/niit/jdp/services/DatabaseService.java
@@ -1,5 +1,18 @@
 package com.niit.jdp.services;
 
-public class DatabaseService {
+import java.sql.Connection;
 
+public class DatabaseService {
+    private final String URL = "";
+    private final String USERNAME = "root";
+    private final String PASSWORD = "root";
+    Connection connection;
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public Connection connect() {
+        return this.connection;
+    }
 }

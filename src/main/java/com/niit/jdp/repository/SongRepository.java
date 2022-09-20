74f16b653693f7416d472d2880f46725589c5bc3
Rishabh <rishabhgupta7199@gmail.com>
2022-09-20 15:37:50 +0530

    added displayAllSong, displaySong, addSongToPlaylist methods

--- src/main/java/com/niit/jdp/repository/SongRepository.java
+++ src/main/java/com/niit/jdp/repository/SongRepository.java
@@ -1,5 +1,17 @@
 package com.niit.jdp.repository;
 
-public class SongRepository {
+import java.sql.Connection;
 
+public class SongRepository {
+    public void displayAllSong(Connection connection) {
+    }
+
+    public void displaySong(Connection connection, int playlistId) {
+    }
+
+    public boolean addSongToPlaylist(Connection connection, int songId, int playlistId) {
+        return false;
+    }
+
+ 
 }

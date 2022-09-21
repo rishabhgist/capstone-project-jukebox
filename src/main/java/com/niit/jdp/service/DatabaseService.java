ec3ad7bbae40841140754190c05d9f5b7c9ec0cf
Rishabh <rishabhgupta7199@gmail.com>
2022-09-21 12:56:22 +0530

    added mysql connector dependency and logic in connect method

--- src/main/java/com/niit/jdp/service/DatabaseService.java
+++ src/main/java/com/niit/jdp/service/DatabaseService.java
@@ -1,9 +1,11 @@
 package com.niit.jdp.service;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseService {
-    private static final String URL = "";
+    private static final String URL = "jdbc:mysql://127.0.0.1:3307/jukebox";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "root";
     Connection connection;
@@ -12,7 +14,8 @@ public class DatabaseService {
         return connection;
     }
 
-    public Connection connect() {
+    public Connection connect() throws SQLException {
+        connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         return this.connection;
     }
 }

42397b8a1b7dd967b10a097a4d54c7166f8ca2bd
Rishabh <rishabhgupta7199@gmail.com>
2022-09-21 15:05:27 +0530

    added printCatalogue method in catalogue

--- src/main/java/com/niit/jdp/Main.java
+++ src/main/java/com/niit/jdp/Main.java
@@ -1,13 +1,22 @@
 package com.niit.jdp;
 
+import com.niit.jdp.model.Song;
+import com.niit.jdp.repository.CatalogueRepository;
 import com.niit.jdp.service.DatabaseService;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         try {
             DatabaseService databaseService = new DatabaseService();
+            CatalogueRepository catalogue = new CatalogueRepository();
+            List<Song> songs = catalogue.displayCatalogue(databaseService.getConnection());
+            for (Song song : songs) {
+                System.out.println(song.toString());
+            }
+
         } catch (SQLException e) {
             System.err.println("Unable to connect to Database, " + e.getMessage());
         }
--- src/main/java/com/niit/jdp/repository/CatalogueRepository.java
+++ src/main/java/com/niit/jdp/repository/CatalogueRepository.java
@@ -3,12 +3,39 @@ package com.niit.jdp.repository;
 import com.niit.jdp.model.Song;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CatalogueRepository {
+    Song song;
 
     public List<Song> displayCatalogue(Connection connection) {
-        return null;
+        List<Song> songsList = new ArrayList<>();
+        String query = "SELECT * FROM `jukebox`.`song`";
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet songResult = statement.executeQuery(query);
+            while (songResult.next()) {
+                song = new Song();
+                song.setSongName(songResult.getString("song_name"));
+                song.setSongArtist(songResult.getString("song_artist"));
+                song.setSongAlbum(songResult.getString("song_album"));
+                song.setSongGenre(songResult.getString("song_genre"));
+                song.setSongUrl(songResult.getString("song_url"));
+                song.setSongLength(songResult.getDouble("song_length"));
+                songsList.add(song);
+            }
+        } catch (SQLException e) {
+            System.out.println("Incorrect search query" + e.getMessage());
+        }
+        return songsList;
+    }
+
+    public void printCatalogue(List<Song> songList) {
+
     }
 
     public List<Song> sortByAlphabet() {

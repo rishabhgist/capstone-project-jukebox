a280e87493392df87b0cee2f124d17553a127b37
Rishabh <rishabhgupta7199@gmail.com>
2022-09-22 09:53:56 +0530

    added test case for DatabaseServiceTest

--- src/test/java/com/niit/jdp/service/DatabaseServiceTest.java
+++ src/test/java/com/niit/jdp/service/DatabaseServiceTest.java
@@ -1,20 +1,43 @@
 package com.niit.jdp.service;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 class DatabaseServiceTest {
+    DatabaseService databaseService;
 
     @BeforeEach
     void setUp() {
+        try {
+            databaseService = new DatabaseService();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @AfterEach
     void tearDown() {
+        databaseService = null;
     }
 
     @Test
-    void givenUrlUsernamePasswordTestConnectionSucessfull() {
+    void givenUrlUsernamePasswordTestConnectionSuccessful() throws SQLException {
+        Assertions.assertEquals(true, databaseService.connect());
+    }
+
+    @Test
+    void givenIncorrectValueTestConnectionFail() {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3307/jukebox", "root", "");
+        } catch (SQLException e) {
+            String message = e.getMessage();
+            Assertions.assertEquals("Access denied for user 'root'@'localhost' (using password: NO)", message);
+        }
     }
 }
